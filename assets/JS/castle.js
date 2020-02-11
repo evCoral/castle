4ffcdb7c1cff4d97d92ab7e607209a6037606aca
evCoral <earlvincentcoral@gmail.com>
2020-02-11 09:47:02 +0800

    Added texture and Moat

--- assets/JS/castle.js
+++ assets/JS/castle.js
@@ -1,28 +1,15 @@
 let scene = new THREE.Scene();
-let camera = new THREE.PerspectiveCamera( 945, window.innerWidth / window.innerHeight, 0.5, 1000);
-
-
-//ground
-const widthGround = 130;
-const heightGround = 2;
-const depthGround = 10;
-const groundGeometry = new THREE.BoxBufferGeometry(widthGround, heightGround, depthGround);
-
-let groundMaterial = new THREE.MeshBasicMaterial( {color: 0xB5651D} );
-let ground = new THREE.Mesh( groundGeometry, groundMaterial );
-ground.position.set(0,15,-1);
-scene.add( ground );
-
+let camera = new THREE.PerspectiveCamera( 945, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 
 //Sea
 const widthSea = 75;
 const heightGroundSea = 2;
-const depthGroundSea = 2;
+const depthGroundSea =20;
 const seaGeometry = new THREE.BoxBufferGeometry(widthSea, heightGroundSea, depthGroundSea);
 
-// let texture = new THREE.TextureLoader().load( 'textures/grasss.jpg' );
-let seaMaterial = new THREE.MeshBasicMaterial( {color: 0xadd8e6} );
+let texture = new THREE.TextureLoader().load( 'assets/texture/seaTexture.jpg' );
+let seaMaterial = new THREE.MeshBasicMaterial( {map: texture} );
 let sea = new THREE.Mesh( seaGeometry, seaMaterial );
 sea.position.set(0,9.5,-5);
 scene.add( sea );
@@ -36,8 +23,8 @@ const heightRightTower = 30;
 const radialSegmentsRightTower = 15;
 const rightTowerGeometry = new THREE.CylinderBufferGeometry(radiusTopRightTower, radiusBottomRightTower, 
                                                         heightRightTower, radialSegmentsRightTower);
-
-let rightTowerMaterial = new THREE.MeshBasicMaterial( { color: 0xB5651D} );
+let rightTowerTexture = new THREE.TextureLoader().load( 'assets/texture/castleWall.jpg' );
+let rightTowerMaterial = new THREE.MeshBasicMaterial( {map:rightTowerTexture} );
 let rightTower = new THREE.Mesh( rightTowerGeometry, rightTowerMaterial );
 rightTower.position.x = -30;
 rightTower.position.z = -11;
@@ -51,8 +38,8 @@ const heightMidRTower = 5;
 const radialSegmentsMidRTower = 15;
 const midRTowerGeometry = new THREE.CylinderBufferGeometry(radiusTopMidRTower, radiusBottomMidRTower, 
                                                         heightMidRTower, radialSegmentsMidRTower);
-// let texture = new THREE.TextureLoader().load( 'textures/s.jpg' );
-let midRTowerMaterial = new THREE.MeshBasicMaterial( { color: 0xB5651D} );
+let midBodyTexture = new THREE.TextureLoader().load( 'assets/texture/stonewall.jpg' );
+let midRTowerMaterial = new THREE.MeshBasicMaterial( { map: midBodyTexture} );
 let MidRTower = new THREE.Mesh( midRTowerGeometry, midRTowerMaterial );
 MidRTower.position.x = -30;
 MidRTower.position.z = -11;
@@ -65,7 +52,8 @@ const heightRightTowerRoof = -9;
 const segmentsRightTowerRoof = 16;
 const RightTowerRoofgeometry = new THREE.ConeBufferGeometry(radiusRightTowerRoof, heightRightTowerRoof, segmentsRightTowerRoof);
 
-let rightTowerRoofMaterial = new THREE.MeshBasicMaterial( { color: 0x000080} );
+let leftRoofTower = new THREE.TextureLoader().load( 'assets/texture/roofTexture.jpg' );
+let rightTowerRoofMaterial = new THREE.MeshBasicMaterial( { map: leftRoofTower} );
 let rigthTowerRoof = new THREE.Mesh( RightTowerRoofgeometry, rightTowerRoofMaterial );
 rigthTowerRoof.position.x = -28;
 rigthTowerRoof.position.z = -11;
@@ -79,7 +67,8 @@ const heightLeftTower = 24;
 const radialSegmentsLeftTower = 15;
 const leftTowerGeometry = new THREE.CylinderBufferGeometry(radiusTopLeftTower, radiusBottomLeftTower, heightLeftTower, radialSegmentsLeftTower);
 
-let leftTowerMaterial = new THREE.MeshBasicMaterial( {color: 0xB5651D} );
+let leftTowerTexture = new THREE.TextureLoader().load( 'assets/texture/castleWall.jpg' );
+let leftTowerMaterial = new THREE.MeshBasicMaterial( {map:leftTowerTexture} );
 let leftTower = new THREE.Mesh( leftTowerGeometry, leftTowerMaterial );
 leftTower.position.x = 32;
 leftTower.position.z = -11;
@@ -107,7 +96,8 @@ const heightLeftTowerRoof = -9;
 const segmentsLefttTowerRoof = 16;
 const LeftTowerRoofgeometry = new THREE.ConeBufferGeometry(radiusLeftTowerRoof, heightLeftTowerRoof, segmentsLefttTowerRoof);
 
-let LeftTowerRoofMaterial = new THREE.MeshBasicMaterial( { color: 0x000080} );
+let rightRoofTexture = new THREE.TextureLoader().load( 'assets/texture/roofTexture.jpg' );
+let LeftTowerRoofMaterial = new THREE.MeshBasicMaterial( { map: rightRoofTexture} );
 let LeftTowerRoof = new THREE.Mesh( LeftTowerRoofgeometry, LeftTowerRoofMaterial );
 LeftTowerRoof.position.x = 30;
 LeftTowerRoof.position.z = -11;
@@ -122,42 +112,45 @@ const heightCastle = 40;
 const depthCastle = 2;
 const castleGeometry = new THREE.BoxBufferGeometry(widthCastle, heightCastle, depthCastle);
 
-
-let castleMaterial = new THREE.MeshBasicMaterial( {color: 0xB5651D} );
+let castleTexture = new THREE.TextureLoader().load( 'assets/texture/castleWall.jpg' );
+let castleMaterial = new THREE.MeshBasicMaterial( {map: castleTexture} );
 let castle = new THREE.Mesh( castleGeometry, castleMaterial );
 castle.position.set(0,-2,-20);
 scene.add( castle );
 
 
 //bridge
-const widthBridge = 15;
+const widthBridge = 25;
 const heightBridge = 2;
-const depthBridge = 5;
+const depthBridge = 17;
 const bridgeeGeometry = new THREE.BoxBufferGeometry(widthBridge, heightBridge, depthBridge);
 
-
-let bridgeMaterial = new THREE.MeshBasicMaterial( {color: 0xB5651D} );
+let bridgeTexture = new THREE.TextureLoader().load( 'assets/texture/bridgeTexture.jpg' );
+let bridgeMaterial = new THREE.MeshBasicMaterial( {map: bridgeTexture} );
 let bridge = new THREE.Mesh( bridgeeGeometry, bridgeMaterial );
-bridge.position.set(0,9,-5);
+bridge.position.set(0,9,-5.5);
 scene.add( bridge );
 
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+camera.position.x = 2;
+camera.position.y = 2;
+
 function animate() {
    requestAnimationFrame( animate );
-   render();
-}
-camera.position.x = 20;
-//Rotation function
-function render() {
-   let speed = Date.now() * 0.00029;
-   camera.position.x = Math.cos(speed) * 10;
-   camera.position.z = Math.sin(speed) * 10;
-   camera.lookAt(scene.position.set(0,0,0));
-
    renderer.render(scene, camera);
+}
 
- }
+//Rotation function
+// function render() {                 
+//    let speed = Date.now() * 0.00029;
+//    camera.position.x = Math.cos(speed) * 10;
+//    camera.position.z = Math.sin(speed) * 10;
+//    camera.lookAt(scene.position.set(0,0,0));
+
+//    renderer.render(scene, camera);
+
+//  }
 animate();
